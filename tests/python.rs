77e730eac0b155fdb805bd825760bc69e98492d0
Baeo Maltinksy <baeo.maltinsky@gmail.com>
2019-04-25 18:51:39 -0700

    Add missing file

---
+++ tests/python.rs
@@ -0,0 +1,18 @@
+use common_utils::kson_strategy::*;
+use kson::Kson;
+use proptest::prelude::*;
+use pyo3::prelude::*;
+use rug::Integer;
+
+proptest! {
+    #[test]
+    fn python(val in arb_kson()) {
+        let gil = Python::acquire_gil();
+        let py = gil.python();
+
+        let py_val = val.to_object(py);
+        let unpy_val: Kson = py_val.extract(py).unwrap();
+
+        assert_eq!(val, unpy_val);
+    }
+}
